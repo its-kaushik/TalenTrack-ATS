f924e642c36550085b10c21d0acd2a00e33d6666
its-kaushik <mkaushik0033@gmail.com>
2023-07-23 16:00:47 +0530

    Creating the register page

---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,341 @@
+import React, { useState } from "react";
+
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import {
+  Alert,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Stack,
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import axios from "axios";
+import { baseUrl } from "../../Utils/constants";
+import { useNavigate } from "react-router";
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="/">
+        TalenTrack
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+function timeout(delay) {
+  return new Promise((res) => setTimeout(res, delay));
+}
+
+const isEmail = (email) =>
+  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
+
+const Register = () => {
+  const navigate = useNavigate();
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  //Inputs
+  const [usernameInput, setUsernameInput] = useState();
+  const [emailInput, setEmailInput] = useState();
+  const [passwordInput, setPasswordInput] = useState();
+  const [companyInput, setCompanyInput] = useState();
+
+  // Inputs Errors
+  const [usernameError, setUsernameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  // Overall Form Validity
+  const [formValid, setFormValid] = useState();
+  const [success, setSuccess] = useState();
+
+  // Handles Display and Hide Password
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  // Validation for onBlur Username
+  const handleUsername = () => {
+    if (!usernameInput) {
+      setUsernameError(true);
+      return;
+    }
+
+    setUsernameError(false);
+  };
+
+  const handleEmail = () => {
+    console.log(isEmail(emailInput));
+    if (!isEmail(emailInput)) {
+      setEmailError(true);
+      return;
+    }
+
+    setEmailError(false);
+  };
+
+  // Validation for onBlur Password
+  const handlePassword = () => {
+    if (
+      !passwordInput ||
+      passwordInput.length < 5 ||
+      passwordInput.length > 20
+    ) {
+      setPasswordError(true);
+      return;
+    }
+
+    setPasswordError(false);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setSuccess(null);
+    //First of all Check for Errors
+
+    // IF username error is true
+    if (usernameError || !usernameInput) {
+      setFormValid("Please Enter Name");
+      return;
+    }
+
+    // If Email error is true
+    if (emailError || !emailInput) {
+      setFormValid("Email is Invalid. Please Re-Enter");
+      return;
+    }
+
+    // If Password error is true
+    if (passwordError || !passwordInput) {
+      setFormValid(
+        "Password is set btw 5 - 20 characters long. Please Re-Enter"
+      );
+      return;
+    }
+    setFormValid(null);
+
+    try {
+      const formData = new FormData();
+
+      formData.append("name", usernameInput);
+      formData.append("email", emailInput);
+      formData.append("password", passwordInput);
+      formData.append("company", companyInput);
+
+      const response = await axios.post(`${baseUrl}/auth/register`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      setSuccess("Form Submitted Successfully");
+
+      await timeout(1500); //for 1 sec delay
+
+      navigate("/login");
+    } catch (err) {
+      if (err.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+
+        setFormValid(err.response.data.message);
+      } else if (err.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+
+        console.log(err.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", err.message);
+      }
+    }
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                autoComplete="name"
+                name="name"
+                required
+                fullWidth
+                id="name"
+                label="Name"
+                autoFocus
+                error={usernameError}
+                value={usernameInput}
+                onChange={(event) => {
+                  setUsernameInput(event.target.value);
+                }}
+                onBlur={handleUsername}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                error={emailError}
+                value={emailInput}
+                onBlur={handleEmail}
+                onChange={(event) => {
+                  setEmailInput(event.target.value);
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              {/* <TextField
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+              /> */}
+              <FormControl variant="outlined" fullWidth required>
+                <InputLabel error={passwordError} htmlFor="password">
+                  Password
+                </InputLabel>
+                <OutlinedInput
+                  margin="normal"
+                  error={passwordError}
+                  onBlur={handlePassword}
+                  name="password"
+                  label="Password"
+                  id="password"
+                  type={showPassword ? "text" : "password"}
+                  onChange={(event) => {
+                    setPasswordInput(event.target.value);
+                  }}
+                  value={passwordInput}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="company"
+                label="Company Name"
+                name="company"
+                autoComplete="company-name"
+                onChange={(event) => {
+                  setCompanyInput(event.target.value);
+                }}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
+
+          {/* Show Form Error if any */}
+          {formValid && (
+            <Stack sx={{ width: "100%", paddingTop: "10px" }} spacing={2}>
+              <Alert severity="error" size="small">
+                {formValid}
+              </Alert>
+            </Stack>
+          )}
+
+          {/* Show Success if no issues */}
+          {success && (
+            <Stack sx={{ width: "100%", paddingTop: "10px" }} spacing={2}>
+              <Alert severity="success" size="small">
+                {success}
+              </Alert>
+            </Stack>
+          )}
+
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+      <Copyright sx={{ mt: 5 }} />
+    </Container>
+  );
+};
+
+export default Register;
+
+/* <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="family-name"
+              />
+            </Grid> */
+
+/* <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid> */
