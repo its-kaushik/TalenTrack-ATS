eede62f5892bf40ea2a7284b9564c88aa0c4b63b
its-kaushik <mkaushik0033@gmail.com>
2023-07-23 13:43:03 +0530

    Improved error handling

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -140,13 +140,27 @@ const Login = () => {
 
       setSuccess("Form Submitted Successfully");
 
-      await timeout(4000); //for 1 sec delay
+      await timeout(1500); //for 1 sec delay
 
       navigate("/home");
 
       //setFormValid("Issue with login");
     } catch (err) {
-      setFormValid(err.response.data.message);
+      if (err.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+
+        setFormValid(err.response.data.message);
+      } else if (err.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+
+        console.log(err.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", err.message);
+      }
     }
 
     //console.log("Email : " + emailInput);
