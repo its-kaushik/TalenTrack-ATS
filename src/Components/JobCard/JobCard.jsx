e5c273d6480b492b5c61e30d40905797336edf6a
its-kaushik <mkaushik0033@gmail.com>
2023-07-25 16:54:05 +0530

    Created the Edit job Modal

--- src/Components/JobCard/JobCard.jsx
+++ src/Components/JobCard/JobCard.jsx
@@ -7,12 +7,26 @@ import {
   CardHeader,
   Collapse,
   IconButton,
+  Modal,
   Typography,
 } from "@mui/material";
 import { ModeEdit } from "@mui/icons-material";
 import { useState } from "react";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { styled } from "@mui/material/styles";
+import EditJobModal from "../EditJobModal/EditJobModal";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  //width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const JobCard = ({
   desc,
@@ -25,6 +39,11 @@ const JobCard = ({
   company,
 }) => {
   const [expanded, setExpanded] = useState(false);
+
+  const [openJobModal, setOpenJobModal] = useState(false);
+
+  const handleModalOpen = () => setOpenJobModal(true);
+  const handleModalClose = () => setOpenJobModal(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
@@ -92,10 +111,16 @@ const JobCard = ({
       </CardContent>
       {!expanded && (
         <CardActions disableSpacing>
-          <IconButton href={`edit/${id}`}>
+          <IconButton onClick={handleModalOpen}>
             <ModeEdit />
           </IconButton>
           <Button href={`${id}`}>View</Button>
+
+          <Modal open={openJobModal} onClose={handleModalClose}>
+            <Box sx={style}>
+              <EditJobModal jobID={id} />
+            </Box>
+          </Modal>
 
           <ExpandMore
             expand={expanded}
@@ -134,10 +159,16 @@ const JobCard = ({
       </Collapse>
       {expanded && (
         <CardActions disableSpacing>
-          <IconButton href={`edit/${id}`}>
+          <IconButton onClick={handleModalOpen}>
             <ModeEdit />
           </IconButton>
           <Button href={`${id}`}>View</Button>
+
+          <Modal open={openJobModal} onClose={handleModalClose}>
+            <Box sx={style}>
+              <EditJobModal jobID={id} />
+            </Box>
+          </Modal>
 
           <ExpandMore
             expand={expanded}
